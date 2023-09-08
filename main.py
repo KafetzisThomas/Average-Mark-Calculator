b9ebdd707b1d1c986c2a7ef1b90603f6cfc5c014
KafetzisThomas <tomkafetzis06@gmail.com>
2023-09-08 19:09:40 +0300

    Enhance code structure

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-# Average Mark Calculator
-# Author: KafetzisThomas - https://github.com/KafetzisThomas
+# Project Title: Average-Mark-Calculator (https://github.com/KafetzisThomas/Average-Mark-Calculator)
+# Author / Project Owner: KafetzisThomas (https://github.com/KafetzisThomas)
 
-subjects = {}
-count = int(input("\nHow many subjects do you have?\n> "))
+def get_subject_grades(**subjects):
+    """Return a dict of subjects associated with grades"""
+    count = int(input("\nHow many subjects do you have?\n> "))
+    for i in range(count):
+        subject = str(input(f"\nSubject {i+1}: "))
+        grade = float(input("Grade: "))
+        subjects[subject] = grade
+        print(f"Added: {subject}")
 
-i = 0
-while i != count:
-  i += 1
-  subject = str(input(f"\nSubject {i}: "))
-  grade = float(input("Grade: "))
-  subjects[subject] = grade
-  print(f"Added: {subject}")
+    return subjects
 
-values = subjects.values()
-total = sum(values) / count
-print(f"\nAverage grade: {total}")
+def calculate_average_grade(subjects):
+    """Return average grade by dividing the sum of grades"""
+    values = subjects.values()
+    total = sum(values) / len(values)
+    return total
+
+
+subjects = get_subject_grades()
+average_grade = calculate_average_grade(subjects)
+print(f"\nAverage grade: {average_grade:.1f}")
